d7d705b9ecd83bef4db47f2cd88b62084dbbf42e
OlojaMuyiwa <olojam4969@gmail.com>
2023-05-15 20:17:55 +0100

    Add ejs package and create Auth module and files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+  /*
+     Here it's assumed that public and views are in the root directory,
+     alongside src. You can put them wherever you want, 
+     just use the correct path if you use another folder.
+  */
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+  await app.listen(8080);
 }
 bootstrap();
